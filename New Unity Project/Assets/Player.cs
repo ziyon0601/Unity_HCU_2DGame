d153b002de5387e4fcfe056e139841f49cb60018
ziyon0601 <58834307+ziyon0601@users.noreply.github.com>
2019-12-13 15:02:12 +0800

    子彈飛

--- New Unity Project/Assets/Player.cs
+++ New Unity Project/Assets/Player.cs
@@ -10,11 +10,60 @@ public class Player : MonoBehaviour
 public float speed = 1.5f;
 [Header("血量"),Range(0,1000)]
 public float hp = 100;
+[Header("子彈物件")]
+public GameObject bullte;
+[Header("發射音效")]
+public AudioClip soundFire;
+
+public Transform point;
 
 
-public Animator ani;
-public Rigidbody2D r2d;
+private Animator ani;
+private Rigidbody2D r2d;
+private AudioSource aud;
 #endregion
-	
+
+public float speedBullet = 1000;
+
+private void Start()
+{
+    ani = GetComponent<Animator>();    //動畫元件 = 取得元件
+    r2d = GetComponent<Rigidbody2D>();
+    aud = GetComponent<AudioSource>();
+}
+
+//一秒執行50次
+private void FixedUpdate()
+{
+    Move();
+}
+
+private void Update()
+{
+    Fire();
+}
+
+private void Move()
+{
+    float h = Input.GetAxisRaw("Horizontal");
+    r2d.AddForce(new Vector2(speed*h,0));
+    if (h != 0) transform.localScale = new Vector3(h,1,1);
+    
+        
+    
+}
+
+private void Fire()
+{
+    if (Input.GetKeyDown(KeyCode.Mouse0))
+    {
+        aud.PlayOneShot(soundFire);
+        GameObject temp = Instantiate(bullte, point.position, Quaternion.identity);
+        temp.GetComponent<Rigidbody2D>().AddForce(new Vector2(speedBullet*transform.localScale.x,80));
+
+        
+     }
+}
+ 	
 }
 
